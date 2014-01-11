e155d5c81620804e6faed7f1ad5470fbc2506c30
Hannes Ebner <hannes@metasolutions.se>
2014-01-11 15:29:48 +0100

    added license and author information

--- modules/transforms/common/src/main/java/org/entrystore/transforms/Pipeline.java
+++ modules/transforms/common/src/main/java/org/entrystore/transforms/Pipeline.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2007-2014
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.entrystore.transforms;
 
 import org.entrystore.repository.Entry;
@@ -23,6 +39,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * @author Matthias Palmér
+ * @author Hannes Ebner
+ */
 public class Pipeline {
 
 	private static Logger log = LoggerFactory.getLogger(Pipeline.class);
