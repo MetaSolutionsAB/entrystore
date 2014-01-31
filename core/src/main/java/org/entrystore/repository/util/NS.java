32770d7099148b9f117fd0c3b7f1d3425da38b70
Hannes Ebner <hannes@metasolutions.se>
2014-01-31 23:13:09 +0100

    removed NS.sc completely instead of deprecating it

--- core/src/main/java/org/entrystore/repository/util/NS.java
+++ core/src/main/java/org/entrystore/repository/util/NS.java
@@ -41,9 +41,6 @@ public class NS {
 
 	public static String rdfs = "http://www.w3.org/2000/01/rdf-schema#";
 
-	//@Deprecated
-	//public static String sc = "http://scam.sf.net/schema#";
-
 	public static String entrystore = "http://entrystore.org/terms/";
 
 	public static String xsd = "http://www.w3.org/2001/XMLSchema#";
