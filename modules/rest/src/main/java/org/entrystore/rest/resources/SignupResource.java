ece483af1bc21eb6a00228082f1cdd32c53e74e2
Hannes Ebner <hannes@metasolutions.se>
2015-02-16 23:01:35 +0100

    added support for JSON

--- modules/rest/src/main/java/org/entrystore/rest/resources/SignupResource.java
+++ modules/rest/src/main/java/org/entrystore/rest/resources/SignupResource.java
@@ -33,6 +33,8 @@ import org.entrystore.rest.auth.Signup;
 import org.entrystore.rest.auth.SignupInfo;
 import org.entrystore.rest.auth.SignupTokenCache;
 import org.entrystore.rest.util.SimpleHTML;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.restlet.data.Form;
 import org.restlet.data.Language;
 import org.restlet.data.MediaType;
@@ -45,6 +47,7 @@ import org.restlet.resource.ResourceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URLDecoder;
@@ -141,43 +144,81 @@ public class SignupResource extends BaseResource {
 
 	@Post
 	public void acceptRepresentation(Representation r) {
-		Form form = new Form(getRequest().getEntity());
-		String firstName = form.getFirstValue("firstname", true);
-		String lastName = form.getFirstValue("lastname", true);
-		String email = form.getFirstValue("email", true);
-		String password = form.getFirstValue("password", true);
-		String rcChallenge = form.getFirstValue("recaptcha_challenge_field", true);
-		String rcResponse = form.getFirstValue("recaptcha_response_field", true);
-		String urlFailure = form.getFirstValue("urlfailure", true);
-		String urlSuccess = form.getFirstValue("urlsuccess", true);
+		SignupInfo ci = new SignupInfo();
+		ci.expirationDate = new Date(new Date().getTime() + (24 * 3600 * 1000)); // 24 hours later
+		String rcChallenge = null;
+		String rcResponse = null;
 
-		if (firstName == null || lastName == null || email == null || password == null) {
+		if (MediaType.APPLICATION_JSON.equals(r.getMediaType())) {
+			try {
+				JSONObject siJson = new JSONObject(r.getText());
+				if (siJson.has("firstname")) {
+					ci.firstName = siJson.getString("firstname");
+				}
+				if (siJson.has("lastname")) {
+					ci.lastName = siJson.getString("lastname");
+				}
+				if (siJson.has("email")) {
+					ci.email = siJson.getString("email");
+				}
+				if (siJson.has("password")) {
+					ci.password = siJson.getString("password");
+				}
+				if (siJson.has("recaptcha_challenge_field")) {
+					rcChallenge = siJson.getString("recaptcha_challenge_field");
+				}
+				if (siJson.has("recaptcha_response_field")) {
+					rcResponse = siJson.getString("recaptcha_response_field");
+				}
+				if (siJson.has("urlfailure")) {
+					ci.urlFailure = siJson.getString("urlfailure");
+				}
+				if (siJson.has("urlsuccess")) {
+					ci.urlSuccess = siJson.getString("urlsuccess");
+				}
+			} catch (Exception e) {
+				getResponse().setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+				return;
+			}
+		} else {
+			Form form = new Form(getRequest().getEntity());
+			ci.firstName = form.getFirstValue("firstname", true);
+			ci.lastName = form.getFirstValue("lastname", true);
+			ci.email = form.getFirstValue("email", true);
+			ci.password = form.getFirstValue("password", true);
+			rcChallenge = form.getFirstValue("recaptcha_challenge_field", true);
+			rcResponse = form.getFirstValue("recaptcha_response_field", true);
+			ci.urlFailure = form.getFirstValue("urlfailure", true);
+			ci.urlSuccess = form.getFirstValue("urlsuccess", true);
+		}
+
+		if (ci.firstName == null || ci.lastName == null || ci.email == null || ci.password == null) {
 			getResponse().setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
 			getResponse().setEntity(html.representation("One or more parameters are missing."));
 			return;
 		}
 
-		if (firstName.trim().length() < 2 || lastName.trim().length() < 2) {
+		if (ci.firstName.trim().length() < 2 || ci.lastName.trim().length() < 2) {
 			getResponse().setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
 			getResponse().setEntity(html.representation("Invalid name."));
 			return;
 		}
 
-		if (password.trim().length() < 8) {
+		if (ci.password.trim().length() < 8) {
 			getResponse().setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
 			getResponse().setEntity(html.representation("The password has to consist of at least 8 characters."));
 			return;
 		}
 
-		if (!EmailValidator.getInstance().isValid(email)) {
+		if (!EmailValidator.getInstance().isValid(ci.email)) {
 			getResponse().setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
-			getResponse().setEntity(html.representation("Invalid email address: " + email));
+			getResponse().setEntity(html.representation("Invalid email address: " + ci.email));
 			return;
 		}
 
 		Config config = getRM().getConfiguration();
 
-		log.info("Received sign-up request for " + email);
+		log.info("Received sign-up request for " + ci.email);
 
 		if ("on".equalsIgnoreCase(config.getString(Settings.AUTH_RECAPTCHA, "off"))
 				&& config.getString(Settings.AUTH_RECAPTCHA_PRIVATE_KEY) != null) {
@@ -186,7 +227,7 @@ public class SignupResource extends BaseResource {
 				getResponse().setEntity(html.representation("reCaptcha information missing"));
 				return;
 			}
-			log.info("Checking reCaptcha for " + email);
+			log.info("Checking reCaptcha for " + ci.email);
 
 			String remoteAddr = getRequest().getClientInfo().getUpstreamAddress();
 			ReCaptchaImpl captcha = new ReCaptchaImpl();
@@ -194,38 +235,29 @@ public class SignupResource extends BaseResource {
 			ReCaptchaResponse reCaptchaResponse = captcha.checkAnswer(remoteAddr, rcChallenge, rcResponse);
 
 			if (reCaptchaResponse.isValid()) {
-				log.info("Valid reCaptcha for " + email);
+				log.info("Valid reCaptcha for " + ci.email);
 			} else {
-				log.info("Invalid reCaptcha for " + email);
+				log.info("Invalid reCaptcha for " + ci.email);
 				getResponse().setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
 				getResponse().setEntity(html.representation("Invalid reCaptcha received."));
 				return;
 			}
 		}
 
-		SignupInfo ci = new SignupInfo();
-		ci.firstName = firstName;
-		ci.lastName = lastName;
-		ci.email = email;
-		ci.password = password;
-		ci.urlFailure = urlFailure;
-		ci.urlSuccess = urlSuccess;
-		ci.expirationDate = new Date(new Date().getTime() + (24 * 3600 * 1000)); // 24 hours later
-
 		String token = RandomStringUtils.randomAlphanumeric(16);
 		String confirmationLink = getRM().getRepositoryURL().toExternalForm() + "auth/signup?confirm=" + token;
 		SignupTokenCache.getInstance().addToken(token, ci);
-		log.info("Generated sign-up token " + token + " for " + email);
+		log.info("Generated sign-up token " + token + " for " + ci.email);
 
-		boolean sendSuccessful = Signup.sendRequestForConfirmation(getRM().getConfiguration(), firstName + " " + lastName, email, confirmationLink, false);
+		boolean sendSuccessful = Signup.sendRequestForConfirmation(getRM().getConfiguration(), ci.firstName + " " + ci.lastName, ci.email, confirmationLink, false);
 		if (sendSuccessful) {
-			log.info("Sent confirmation request to " + email);
+			log.info("Sent confirmation request to " + ci.email);
 		} else {
-			log.info("Failed to send confirmation request to " + email);
+			log.info("Failed to send confirmation request to " + ci.email);
 		}
 
 		getResponse().setStatus(Status.SUCCESS_OK);
-		getResponse().setEntity(html.representation("A confirmation message was sent to " + email));
+		getResponse().setEntity(html.representation("A confirmation message was sent to " + ci.email));
 	}
 
 	private String constructHtmlForm(boolean reCaptcha) {
