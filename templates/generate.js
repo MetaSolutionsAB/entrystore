1bcdaf66de999407c180ff62e1ffe2c251cc6d4f
Hannes Ebner <hannes@metasolutions.se>
2017-12-12 11:50:12 +0100

    Added support for translations

--- templates/generate.js
+++ templates/generate.js
@@ -24,7 +24,14 @@ var minify = function(html) {
     return htmlMinify(html, {removeComments: true, collapseWhitespace: true, minifyCSS: true, keepClosingSlash: true, caseSensitive: true});
 };
 
-var mailGenerator = new Mailgen({
+for (var lang in conf) {
+  if (!conf.hasOwnProperty(lang)) {
+      continue;
+  }
+
+  var langStr = conf[lang];
+
+  var mailGenerator = new Mailgen({
     /*
     theme: {
         path: path.resolve('theme/index.html'),
@@ -33,49 +40,55 @@ var mailGenerator = new Mailgen({
     */
     theme: 'salted',
     product: {
-        name: conf.productName,
-        link: conf.productURL,
-        logo: conf.productLogoURL,
-	copyright: conf.copyright,
+      name: langStr.productName,
+      link: langStr.productURL,
+      logo: langStr.productLogoURL,
+      copyright: langStr.copyright
     }
-});
+  });
 
-var emailSignup = {
+  var emailSignup = {
     body: {
-        name: '__NAME__',
-        intro: 'Welcome to ' + conf.productName + '!',
-        action: {
-            instructions: 'To get started with ' + conf.productName + ', please click here:',
-            button: {
-                color: '#22BC66', // Optional action button color 
-                text: 'Confirm your account',
-                link: '__CONFIRMATION_LINK__'
-            }
-        },
-        outro: conf.supportText,
+      name: '__NAME__',
+      intro: langStr.signupIntro,
+      action: {
+        instructions: langStr.signupInstructions,
+        button: {
+          color: '#22BC66', // Optional action button color
+          text: langStr.signupButton,
+          link: '__CONFIRMATION_LINK__'
+        }
+      },
+      outro: langStr.supportText
     }
-};
+  };
 
-fs.writeFileSync(outDir + 'signup.html', minify(mailGenerator.generate(emailSignup)), 'utf8');
-fs.writeFileSync(outDir + 'signup.txt', mailGenerator.generatePlaintext(emailSignup), 'utf8');
-
-var emailPwReset = {
+  var emailPwReset = {
     body: {
-        intro: 'You have received this email because a password reset request for your ' + conf.productName + ' account was received.',
-        action: {
-            instructions: 'Click the button below to reset your password:',
-            button: {
-                color: '#22BC66', // Optional action button color 
-                text: 'Reset your password',
-                link: '__CONFIRMATION_LINK__'
-            }
-        },
-        outro: [
-            'If you did not request a password reset, no further action is required on your part.',
-            conf.supportText,
-        ],
+      intro: langStr.pwresetIntro,
+      action: {
+        instructions: langStr.pwresetInstructions,
+        button: {
+          color: '#22BC66', // Optional action button color
+          text: langStr.pwresetButton,
+          link: '__CONFIRMATION_LINK__'
+        }
+      },
+      outro: [
+        langStr.pwresetOutro,
+        langStr.supportText
+      ]
     }
-};
+  };
 
-fs.writeFileSync(outDir + 'pwreset.html', minify(mailGenerator.generate(emailPwReset)), 'utf8');
-fs.writeFileSync(outDir + 'pwreset.txt', mailGenerator.generatePlaintext(emailPwReset), 'utf8');
+  if (lang == 'en') {
+    lang = '';
+  } else {
+    lang = '_' + lang;
+  }
+
+  fs.writeFileSync(outDir + 'signup' + lang + '.html', minify(mailGenerator.generate(emailSignup)), 'utf8');
+  fs.writeFileSync(outDir + 'signup' + lang + '.txt', mailGenerator.generatePlaintext(emailSignup), 'utf8');
+  fs.writeFileSync(outDir + 'pwreset' + lang + '.html', minify(mailGenerator.generate(emailPwReset)), 'utf8');
+  fs.writeFileSync(outDir + 'pwreset' + lang + '.txt', mailGenerator.generatePlaintext(emailPwReset), 'utf8');
+}
