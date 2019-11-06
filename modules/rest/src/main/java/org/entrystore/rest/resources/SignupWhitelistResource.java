f764d0ed662721829ae01227bf9aca60efc369de
Hannes Ebner <hannes@metasolutions.se>
2019-11-06 15:00:03 +0100

    Organized imports

--- modules/rest/src/main/java/org/entrystore/rest/resources/SignupWhitelistResource.java
+++ modules/rest/src/main/java/org/entrystore/rest/resources/SignupWhitelistResource.java
@@ -16,49 +16,18 @@
 
 package org.entrystore.rest.resources;
 
-import com.google.common.base.Joiner;
-import net.tanesha.recaptcha.ReCaptcha;
-import net.tanesha.recaptcha.ReCaptchaFactory;
-import net.tanesha.recaptcha.ReCaptchaImpl;
-import net.tanesha.recaptcha.ReCaptchaResponse;
-import org.apache.commons.lang.RandomStringUtils;
-import org.apache.commons.validator.routines.EmailValidator;
-import org.entrystore.Context;
-import org.entrystore.Entry;
-import org.entrystore.GraphType;
-import org.entrystore.PrincipalManager;
-import org.entrystore.User;
 import org.entrystore.config.Config;
 import org.entrystore.repository.config.Settings;
-import org.entrystore.rest.auth.Signup;
-import org.entrystore.rest.auth.SignupInfo;
-import org.entrystore.rest.auth.SignupTokenCache;
-import org.entrystore.rest.util.RecaptchaVerifier;
-import org.entrystore.rest.util.SimpleHTML;
 import org.json.JSONArray;
-import org.json.JSONObject;
-import org.restlet.data.Form;
-import org.restlet.data.Language;
-import org.restlet.data.MediaType;
-import org.restlet.data.Status;
 import org.restlet.ext.json.JsonRepresentation;
-import org.restlet.representation.EmptyRepresentation;
 import org.restlet.representation.Representation;
-import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Get;
-import org.restlet.resource.Post;
 import org.restlet.resource.ResourceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URLDecoder;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 
 /**
