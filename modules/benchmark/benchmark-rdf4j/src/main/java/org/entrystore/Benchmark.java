a8bfe915fca451b1ac93c68ce9da8351692d46ab
patrik <patrik@metasolutions.se>
2024-03-27 14:54:49 +0100

    Adding multiple transactions variation

--- modules/benchmark/benchmark-rdf4j/src/main/java/org/entrystore/Benchmark.java
+++ modules/benchmark/benchmark-rdf4j/src/main/java/org/entrystore/Benchmark.java
@@ -5,10 +5,12 @@ import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.RepositoryResult;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.sail.lmdb.LmdbStore;
 import org.eclipse.rdf4j.sail.memory.MemoryStore;
 import org.eclipse.rdf4j.sail.nativerdf.NativeStore;
 import org.entrystore.model.FakeGenerator;
 import org.entrystore.model.FakePerson;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.time.LocalDateTime;
@@ -17,7 +19,10 @@ import java.util.List;
 public class Benchmark {
 
     private static final File NATIVE_PATH = new File("modules/benchmark/benchmark-rdf4j/src/main/resources/native_store");
-    private static final String NATIVE = "Native";
+    private static final File LMDB_PATH = new File("modules/benchmark/benchmark-rdf4j/src/main/resources/lmdb_store");
+    private static final String NATIVE = "native";
+    private static final String MEMORY = "memory";
+    private static final String LMDB = "lmdb";
 
     private static List<FakePerson> generateData(int sizeToGenerate) {
 
@@ -57,24 +62,19 @@ public class Benchmark {
     public static void main(String[] args) {
 
         try {
+
+            // process arguments
             String benchmarkType = args[0];
             boolean withTransactions = "true".equals(args[1]);
             int sizeToGenerate = Integer.parseInt(args[2]);
+
+            Repository database = getDatabase(benchmarkType);
 
             // welcome message
             LogUtils.logWelcome(benchmarkType, withTransactions, sizeToGenerate);
 
             // generate list of Persons with Addresses, 1 Person has exactly 1 Address
             List<FakePerson> persons = generateData(sizeToGenerate);
-
-            Repository database;
-
-            // choose a storage type NATIVE | MEMORY
-            if (NATIVE.equals(benchmarkType)) {
-                database = new SailRepository(new NativeStore(NATIVE_PATH));
-            } else {
-                database = new SailRepository(new MemoryStore());
-            }
 
             // run either a mutli-transaction or single-transaction benchmark
             try (RepositoryConnection connection = database.getConnection()) {
@@ -99,5 +99,17 @@ public class Benchmark {
         } catch (IllegalArgumentException | ArrayIndexOutOfBoundsException ex) {
             LogUtils.log.warn("No or bad arguments provided.");
         }
+    }
+
+    @NotNull
+    private static Repository getDatabase(String benchmarkType) {
+
+        // choose a storage type NATIVE | MEMORY
+        return switch (benchmarkType) {
+            case MEMORY -> new SailRepository(new MemoryStore());
+            case NATIVE -> new SailRepository(new NativeStore(NATIVE_PATH));
+            case LMDB -> new SailRepository(new LmdbStore(LMDB_PATH));
+            case null, default -> throw new IllegalArgumentException();
+        };
     }
 }
