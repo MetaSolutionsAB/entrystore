57e474d63c09ea5b254b284c6a116af60506007a
Kamil Pozniak <kamil@metasolutions.se>
2024-09-12 21:25:56 +0200

    ENTRYSTORE-823 Add /management/status endpoint tests

--- modules/rest-standalone/integration-test/src/test/groovy/org/entrystore/rest/it/ManagementStatusIT.groovy
+++ modules/rest-standalone/integration-test/src/test/groovy/org/entrystore/rest/it/ManagementStatusIT.groovy
@@ -1,24 +1,46 @@
 package org.entrystore.rest.it
 
+
 import org.entrystore.rest.standalone.EntryStoreApplicationStandaloneJetty
 import spock.lang.Specification
 
 import static java.net.HttpURLConnection.HTTP_OK
+import static java.net.HttpURLConnection.HTTP_UNAUTHORIZED
 
 class ManagementStatusIT extends Specification {
 
-    def "Status endpoint should reply with status"() {
-        given:
-        def args = ['-c', 'file:src/test/resources/entrystore-it.properties'] as String[]
-        EntryStoreApplicationStandaloneJetty.main(args)
-        def connection = (HttpURLConnection) new URI('http://localhost:8181/management/status').toURL().openConnection()
+	static String host = 'localhost'
+	static int port = Math.abs(new Random().nextInt() % 50000) + 10000
+	static String origin = 'http://' + host + ':' + port
 
-        when:
-        connection.connect()
+	def setupSpec() {
+		def args = ['-c', 'file:src/test/resources/entrystore-it.properties', '-p', port.toString()] as String[]
+		EntryStoreApplicationStandaloneJetty.main(args)
+	}
 
-        then:
-        connection.getResponseCode() == HTTP_OK
-        connection.getContentType().contains('text/plain')
-        connection.getInputStream().text == 'DOWN'
-    }
+	def "/management/status should reply with status"() {
+		given:
+		def connection = (HttpURLConnection) new URI(origin + '/management/status').toURL().openConnection()
+
+		when:
+		connection.connect()
+
+		then:
+		connection.getResponseCode() == HTTP_OK
+		connection.getContentType().contains('text/plain')
+		connection.getInputStream().text == 'UP'
+	}
+
+	def "/management/status?extended should reply with Unauthorized error for non-admin user"() {
+		given:
+		def connection = (HttpURLConnection) new URI(origin + '/management/status?extended=true').toURL().openConnection()
+
+		when:
+		connection.connect()
+
+		then:
+		connection.getResponseCode() == HTTP_UNAUTHORIZED
+		connection.getContentType().contains('application/json')
+		connection.getErrorStream().text.contains('"error":"Not authorized"')
+	}
 }
