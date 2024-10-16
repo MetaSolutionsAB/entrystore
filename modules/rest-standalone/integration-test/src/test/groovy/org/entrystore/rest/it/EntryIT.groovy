c88a6fb4ddd13d0c6d23cf61b15627bcb65a7822
Kamil Pozniak <kamil@metasolutions.se>
2024-10-16 18:11:16 +0200

    ENTRYSTORE-823 Add IT for Entry creation from a template

--- modules/rest-standalone/integration-test/src/test/groovy/org/entrystore/rest/it/EntryIT.groovy
+++ modules/rest-standalone/integration-test/src/test/groovy/org/entrystore/rest/it/EntryIT.groovy
@@ -474,4 +474,120 @@ class EntryIT extends BaseSpec {
 		then:
 		connection.getResponseCode() == HTTP_UNAUTHORIZED
 	}
+
+	def "POST /{context-id}?entrytype=link&template=otherEntry with metadata in the body, should create a new link entry with local metadata combined with MD from otherEntry"() {
+		given:
+		def otherEntryParams = [entrytype: 'link', resource: resourceUrl]
+		def newResourceIri = EntryStoreClient.baseUrl + '/' + contextId + '/resource/_newId'
+		def otherEntryBody = [metadata: [(newResourceIri): [
+			(NameSpaceConst.DC_TERM_TITLE)  : [[
+												   type : 'literal',
+												   value: 'Other Entry Title'
+											   ]],
+			(NameSpaceConst.RDF_TYPE)       : [[
+												   type : 'uri',
+												   value: NameSpaceConst.NS_DCAT_DATASET
+											   ]],
+			(NameSpaceConst.DC_TERM_CREATOR): [[
+												   type : 'literal',
+												   value: 'Other Entry Creator'
+											   ]]
+		]]]
+		def otherEntryId = createEntry(contextId, otherEntryParams, otherEntryBody)
+		assert otherEntryId.length() > 0
+
+		def params = [entrytype: 'link', resource: resourceUrl, template: EntryStoreClient.baseUrl + '/' + contextId + '/entry/' + otherEntryId]
+		def body = [metadata: [(newResourceIri): [
+			(NameSpaceConst.DC_TERM_TITLE)    : [[
+													 type : 'literal',
+													 value: 'New Entry Title' // should not get overridden
+												 ]],
+			(NameSpaceConst.RDF_TYPE)         : [[
+													 type : 'uri',
+													 value: NameSpaceConst.TERM_NAMED_RESOURCE  // should get overridden
+												 ]],
+			(NameSpaceConst.DC_TERM_PUBLISHER): [[
+													 type : 'literal',
+													 value: 'New Entry Publisher' // should not get overridden
+												 ]]
+		]]]
+
+		when:
+		def entryId = createEntry(contextId, params, body)
+
+		then:
+		entryId.length() > 0
+
+		// fetch created entry
+		def entryConn = EntryStoreClient.getRequest('/' + contextId + '/entry/' + entryId)
+		entryConn.getResponseCode() == HTTP_OK
+		entryConn.getContentType().contains('application/json')
+		def entryRespJson = (new JsonSlurper()).parseText(entryConn.getInputStream().text)
+		entryRespJson['entryId'] == entryId
+		entryRespJson['info'] != null
+		def entryUri = EntryStoreClient.baseUrl + '/' + contextId + '/entry/' + entryId
+		entryRespJson['info'][entryUri] != null
+
+		entryRespJson['info'][entryUri][NameSpaceConst.RDF_TYPE] != null
+		def entryTypes = entryRespJson['info'][entryUri][NameSpaceConst.RDF_TYPE].collect()
+		entryTypes.size() == 1
+		entryTypes[0]['type'] == 'uri'
+		entryTypes[0]['value'] == NameSpaceConst.TERM_LINK
+
+		entryRespJson['info'][entryUri][NameSpaceConst.TERM_RESOURCE] != null
+		def entryResources = entryRespJson['info'][entryUri][NameSpaceConst.TERM_RESOURCE].collect()
+		entryResources.size() == 1
+		entryResources[0]['type'] == 'uri'
+		entryResources[0]['value'] == resourceUrl
+
+		entryRespJson['info'][entryUri][NameSpaceConst.TERM_METADATA] != null
+		def entryMetadata = entryRespJson['info'][entryUri][NameSpaceConst.TERM_METADATA].collect()
+		entryMetadata.size() == 1
+		entryMetadata[0]['type'] == 'uri'
+		entryMetadata[0]['value'] != null
+		entryMetadata[0]['value'].toString().contains('/store/' + contextId + '/metadata/')
+		def storedMetadataUrl = entryMetadata[0]['value'].toString()
+
+		// fetch entry metadata
+		def entryMetaConn = EntryStoreClient.getRequest(storedMetadataUrl)
+		entryMetaConn.getResponseCode() == HTTP_OK
+		entryMetaConn.getContentType().contains('application/json')
+		def entryMetaRespJson = (new JsonSlurper()).parseText(entryMetaConn.getInputStream().text)
+		def metadataKeys = (entryMetaRespJson as Map).keySet()
+		metadataKeys.size() == 2
+
+		def firstMetadataUri = metadataKeys[0].toString()
+		(entryMetaRespJson[firstMetadataUri] as Map).keySet().size() == 2 // only 2 metadata should be copied from Other entry - without dc:title
+		entryMetaRespJson[firstMetadataUri][NameSpaceConst.DC_TERM_TITLE] == null
+		entryMetaRespJson[firstMetadataUri][NameSpaceConst.DC_TERM_CREATOR] != null
+		def dcCreators = entryMetaRespJson[firstMetadataUri][NameSpaceConst.DC_TERM_CREATOR].collect()
+		dcCreators.size() == 1
+		dcCreators[0]['type'] == 'literal'
+		dcCreators[0]['value'] == 'Other Entry Creator'
+		entryMetaRespJson[firstMetadataUri][NameSpaceConst.RDF_TYPE] != null
+		def rdfTypes = entryMetaRespJson[firstMetadataUri][NameSpaceConst.RDF_TYPE].collect()
+		rdfTypes.size() == 1
+		rdfTypes[0]['type'] == 'uri'
+		rdfTypes[0]['value'] == NameSpaceConst.NS_DCAT_DATASET
+
+		def secondMetadataUri = metadataKeys[1].toString()
+		(entryMetaRespJson[secondMetadataUri] as Map).keySet().size() == 3 // All 3 metadata should be copied from New entry
+		entryMetaRespJson[secondMetadataUri][NameSpaceConst.DC_TERM_TITLE] != null
+		def dcTitles = entryMetaRespJson[secondMetadataUri][NameSpaceConst.DC_TERM_TITLE].collect()
+		dcTitles.size() == 1
+		dcTitles[0]['type'] == 'literal'
+		dcTitles[0]['value'] == 'New Entry Title'
+		entryMetaRespJson[secondMetadataUri][NameSpaceConst.DC_TERM_CREATOR] == null
+		entryMetaRespJson[secondMetadataUri][NameSpaceConst.DC_TERM_PUBLISHER] != null
+		def dcPublishers = entryMetaRespJson[secondMetadataUri][NameSpaceConst.DC_TERM_PUBLISHER].collect()
+		dcPublishers.size() == 1
+		dcPublishers[0]['type'] == 'literal'
+		dcPublishers[0]['value'] == 'New Entry Publisher'
+		entryMetaRespJson[secondMetadataUri][NameSpaceConst.RDF_TYPE] != null
+		def rdf2Types = entryMetaRespJson[secondMetadataUri][NameSpaceConst.RDF_TYPE].collect()
+		rdf2Types.size() == 1
+		rdf2Types[0]['type'] == 'uri'
+		rdf2Types[0]['value'] == NameSpaceConst.TERM_NAMED_RESOURCE
+	}
+
 }
