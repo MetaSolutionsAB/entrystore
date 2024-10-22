67b5b6efc722059756f198ce7829e36637271136
Kamil Pozniak <kamil@metasolutions.se>
2024-10-22 18:50:03 +0200

    ENTRYSTORE-823 Add IT for GET Entry includeAll

--- modules/rest-standalone/integration-test/src/test/groovy/org/entrystore/rest/it/EntryIT.groovy
+++ modules/rest-standalone/integration-test/src/test/groovy/org/entrystore/rest/it/EntryIT.groovy
@@ -5,6 +5,7 @@ import org.entrystore.rest.it.util.EntryStoreClient
 import org.entrystore.rest.it.util.NameSpaceConst
 
 import static java.net.HttpURLConnection.*
+import static java.nio.charset.StandardCharsets.UTF_8
 
 class EntryIT extends BaseSpec {
 
@@ -588,6 +589,175 @@ class EntryIT extends BaseSpec {
 		rdf2Types.size() == 1
 		rdf2Types[0]['type'] == 'uri'
 		rdf2Types[0]['value'] == NameSpaceConst.TERM_NAMED_RESOURCE
+	}
+
+	def "GET /{context-id}/entry/{entry-id}?includeAll for a link entry, should return extra information about the entry"() {
+		given:
+		def params = [entrytype: 'link', resource: resourceUrl]
+		def newResourceIri = EntryStoreClient.baseUrl + '/' + contextId + '/resource/_newId'
+		def body = [metadata: [(newResourceIri): [
+			(NameSpaceConst.DC_TERM_TITLE): [[
+												 type : 'literal',
+												 value: 'Cool entry 20'
+											 ]],
+		]]]
+		def entryId = createEntry(contextId, params, body)
+		assert entryId.length() > 0
+
+		when:
+		def entryConn = EntryStoreClient.getRequest('/' + contextId + '/entry/' + entryId + '?includeAll')
+
+		then:
+		entryConn.getResponseCode() == HTTP_OK
+		entryConn.getContentType().contains('application/json')
+		def entryRespJson = (new JsonSlurper()).parseText(entryConn.getInputStream().text)
+		entryRespJson['entryId'] == entryId
+		entryRespJson['info'] != null
+		def entryUri = EntryStoreClient.baseUrl + '/' + contextId + '/entry/' + entryId
+		entryRespJson['info'][entryUri] != null
+
+		entryRespJson['info'][entryUri][NameSpaceConst.RDF_TYPE] != null
+		def entryTypes = entryRespJson['info'][entryUri][NameSpaceConst.RDF_TYPE].collect()
+		entryTypes.size() == 1
+		entryTypes[0]['type'] == 'uri'
+		entryTypes[0]['value'] == NameSpaceConst.TERM_LINK
+
+		entryRespJson['info'][entryUri][NameSpaceConst.TERM_RESOURCE] != null
+		def entryResources = entryRespJson['info'][entryUri][NameSpaceConst.TERM_RESOURCE].collect()
+		entryResources.size() == 1
+		entryResources[0]['type'] == 'uri'
+		entryResources[0]['value'] == resourceUrl
+
+		entryRespJson['info'][entryUri][NameSpaceConst.TERM_METADATA] != null
+		def entryMetadata = entryRespJson['info'][entryUri][NameSpaceConst.TERM_METADATA].collect()
+		entryMetadata.size() == 1
+		entryMetadata[0]['type'] == 'uri'
+		entryMetadata[0]['value'] != null
+		entryMetadata[0]['value'].toString().contains('/store/' + contextId + '/metadata/')
+		def storedMetadataUrl = entryMetadata[0]['value'].toString()
+
+		entryRespJson['metadata'] != null
+		(entryRespJson['metadata'] as Map).keySet().size() == 1
+		def entryMDResourceUrl = (entryRespJson['metadata'] as Map).keySet()[0].toString()
+		entryRespJson['metadata'][entryMDResourceUrl][NameSpaceConst.DC_TERM_TITLE] != null
+		def entryMDdcTitles = entryRespJson['metadata'][entryMDResourceUrl][NameSpaceConst.DC_TERM_TITLE].collect()
+		entryMDdcTitles.size() == 1
+		entryMDdcTitles[0]['type'] == 'literal'
+		entryMDdcTitles[0]['value'] == 'Cool entry 20'
+
+		entryRespJson['rights'] != null
+		entryRespJson['relations'] != null
+
+		// fetch entry metadata
+		def entryMetaConn = EntryStoreClient.getRequest(storedMetadataUrl)
+		entryMetaConn.getResponseCode() == HTTP_OK
+		entryMetaConn.getContentType().contains('application/json')
+		def entryMetaRespJson = (new JsonSlurper()).parseText(entryMetaConn.getInputStream().text)
+		(entryMetaRespJson as Map).keySet().size() == 1
+		def metaResourceUrl = (entryMetaRespJson as Map).keySet()[0].toString()
+		entryMetaRespJson[metaResourceUrl][NameSpaceConst.DC_TERM_TITLE] != null
+		def dcTitles = entryMetaRespJson[metaResourceUrl][NameSpaceConst.DC_TERM_TITLE].collect()
+		dcTitles.size() == 1
+		dcTitles[0]['type'] == 'literal'
+		dcTitles[0]['value'] == 'Cool entry 20'
+	}
+
+	def "GET /{context-id}/entry/{entry-id}?includeAll for a linkreference entry, should return extra information about the entry"() {
+		given:
+		def metadataUrl = 'https://bbc.co.uk/metadata'
+		def params = [entrytype: 'linkreference', resource: resourceUrl, 'cached-external-metadata': URLEncoder.encode(metadataUrl, UTF_8)]
+		def newResourceIri = EntryStoreClient.baseUrl + '/' + contextId + '/resource/_newId'
+		def body = [metadata: [(newResourceIri): [
+			(NameSpaceConst.DC_TERM_TITLE): [[
+												 type : 'literal',
+												 value: 'local metadata title'
+											 ]]
+		]]]
+		def entryId = createEntry(contextId, params, body)
+		assert entryId.length() > 0
+
+		when:
+		def entryConn = EntryStoreClient.getRequest('/' + contextId + '/entry/' + entryId + '?includeAll')
+
+		then:
+		entryConn.getResponseCode() == HTTP_OK
+		entryConn.getContentType().contains('application/json')
+		def entryRespJson = (new JsonSlurper()).parseText(entryConn.getInputStream().text)
+		entryRespJson['entryId'] == entryId
+		entryRespJson['info'] != null
+		def entryUri = EntryStoreClient.baseUrl + '/' + contextId + '/entry/' + entryId
+		entryRespJson['info'][entryUri] != null
+
+		entryRespJson['info'][entryUri][NameSpaceConst.RDF_TYPE] != null
+		def entryTypes = entryRespJson['info'][entryUri][NameSpaceConst.RDF_TYPE].collect()
+		entryTypes.size() == 1
+		entryTypes[0]['type'] == 'uri'
+		entryTypes[0]['value'] == NameSpaceConst.TERM_LINK_REFERENCE
+
+		entryRespJson['info'][entryUri][NameSpaceConst.TERM_RESOURCE] != null
+		def entryResources = entryRespJson['info'][entryUri][NameSpaceConst.TERM_RESOURCE].collect()
+		entryResources.size() == 1
+		entryResources[0]['type'] == 'uri'
+		entryResources[0]['value'] == resourceUrl
+
+		entryRespJson['info'][entryUri][NameSpaceConst.TERM_METADATA] != null
+		def entryMetadata = entryRespJson['info'][entryUri][NameSpaceConst.TERM_METADATA].collect()
+		entryMetadata.size() == 1
+		entryMetadata[0]['type'] == 'uri'
+		entryMetadata[0]['value'] != null
+		entryMetadata[0]['value'].toString().contains('/store/' + contextId + '/metadata/')
+		def storedMetadataUrl = entryMetadata[0]['value'].toString()
+
+		entryRespJson['metadata'] != null
+		(entryRespJson['metadata'] as Map).keySet().size() == 1
+		def entryMDResourceUrl = (entryRespJson['metadata'] as Map).keySet()[0].toString()
+		entryRespJson['metadata'][entryMDResourceUrl][NameSpaceConst.DC_TERM_TITLE] != null
+		def entryMDdcTitles = entryRespJson['metadata'][entryMDResourceUrl][NameSpaceConst.DC_TERM_TITLE].collect()
+		entryMDdcTitles.size() == 1
+		entryMDdcTitles[0]['type'] == 'literal'
+		entryMDdcTitles[0]['value'] == 'local metadata title'
+
+		entryRespJson['cached-external-metadata'] != null
+		entryRespJson['cached-external-metadata'] as Map == [:]
+
+		entryRespJson['rights'] != null
+		entryRespJson['rights'].collect() == ['administer']
+		entryRespJson['relations'] != null
+		entryRespJson['relations'] as Map == [:]
+
+		// fetch entry metadata
+		def entryMetaConn = EntryStoreClient.getRequest(storedMetadataUrl)
+		entryMetaConn.getResponseCode() == HTTP_OK
+		entryMetaConn.getContentType().contains('application/json')
+		def entryMetaRespJson = (new JsonSlurper()).parseText(entryMetaConn.getInputStream().text)
+		(entryMetaRespJson as Map).keySet().size() == 1
+		def metaResourceUrl = (entryMetaRespJson as Map).keySet()[0].toString()
+		entryMetaRespJson[metaResourceUrl][NameSpaceConst.DC_TERM_TITLE] != null
+		def dcTitles = entryMetaRespJson[metaResourceUrl][NameSpaceConst.DC_TERM_TITLE].collect()
+		dcTitles.size() == 1
+		dcTitles[0]['type'] == 'literal'
+		dcTitles[0]['value'] == 'local metadata title'
+
+		entryRespJson['info'][entryUri][NameSpaceConst.TERM_EXTERNAL_METADATA] != null
+		def entryExtMetadata = entryRespJson['info'][entryUri][NameSpaceConst.TERM_EXTERNAL_METADATA].collect()
+		entryExtMetadata.size() == 1
+		entryExtMetadata[0]['type'] == 'uri'
+		entryExtMetadata[0]['value'] == metadataUrl
+
+		entryRespJson['info'][entryUri][NameSpaceConst.TERM_CACHED_EXTERNAL_METADATA] != null
+		def entryCachedExtMetadata = entryRespJson['info'][entryUri][NameSpaceConst.TERM_CACHED_EXTERNAL_METADATA].collect()
+		entryCachedExtMetadata.size() == 1
+		entryCachedExtMetadata[0]['type'] == 'uri'
+		entryCachedExtMetadata[0]['value'] != null
+		entryCachedExtMetadata[0]['value'].toString().contains('/store/' + contextId + '/cached-external-metadata/')
+		def storedExternalMetadataUrl = entryCachedExtMetadata[0]['value'].toString()
+
+		// fetch external metadata
+		def entryExternalMetaConn = EntryStoreClient.getRequest(storedExternalMetadataUrl)
+		entryExternalMetaConn.getResponseCode() == HTTP_OK
+		entryExternalMetaConn.getContentType().contains('application/json')
+		def entryExternalMetaRespJson = (new JsonSlurper()).parseText(entryExternalMetaConn.getInputStream().text)
+		(entryExternalMetaRespJson as Map).keySet().size() == 0
 	}
 
 }
