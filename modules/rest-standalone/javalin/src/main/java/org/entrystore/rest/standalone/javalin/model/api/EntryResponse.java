3d165ff286216ea24724ef081e0155711d5ee0d3
Kamil Pozniak <kamil@metasolutions.se>
2024-12-23 18:54:50 +0100

    ENTRYSTORE-845 Javalin - Add endpoint with path parameter

---
+++ modules/rest-standalone/javalin/src/main/java/org/entrystore/rest/standalone/javalin/model/api/EntryResponse.java
@@ -0,0 +1,4 @@
+package org.entrystore.rest.standalone.javalin.model.api;
+
+public record EntryResponse(String entryId, String type) {
+}
--- modules/rest-standalone/javalin/src/main/java/org/entrystore/rest/standalone/javalin/server/WebServer.java
+++ modules/rest-standalone/javalin/src/main/java/org/entrystore/rest/standalone/javalin/server/WebServer.java
@@ -1,6 +1,9 @@
 package org.entrystore.rest.standalone.javalin.server;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
+import org.entrystore.rest.standalone.javalin.model.api.EntryResponse;
 import org.entrystore.rest.standalone.javalin.model.api.StatusResponse;
 
 import java.io.IOException;
@@ -9,14 +12,45 @@ import java.util.Properties;
 
 public class WebServer {
 
-	public WebServer() throws IOException {
+	final XmlMapper xmlMapper = new XmlMapper();
 
-		Properties props = loadProperties();
+	public WebServer() {
 
-		Javalin.create(/*config*/)
-			.get("/management/status", ctx ->
-				ctx.json(new StatusResponse(props.getProperty("app.version"), "")))
+		Javalin.create(/* Javalin config */)
+			.get("/management/status", this::handleStatusResponseBasedOnAcceptHeader)
+			.get("/entries/{entry-id}", this::handleEntityResponse)
 			.start(8080);
+	}
+
+	private void handleStatusResponseBasedOnAcceptHeader(Context ctx) throws Exception {
+		Properties props = loadProperties();
+		StatusResponse responseModel = new StatusResponse(props.getProperty("app.version"), "online");
+
+		// Check the Accept header
+		String acceptHeader = ctx.header("Accept");
+
+		if (acceptHeader != null && acceptHeader.contains("application/xml")) {
+			// Respond with XML
+			ctx.contentType("application/xml");
+			ctx.result(convertObjectToXml(responseModel));
+		} else {
+			// Default to JSON
+			ctx.json(responseModel);
+		}
+	}
+
+	private void handleEntityResponse(Context ctx) {
+
+		// get the path param
+		// here I'm just testing ID as Integer - checking if 400 is thrown when non-digit value is passed
+		String entryId = ctx.pathParamAsClass("entry-id", Integer.class).get().toString();
+
+		EntryResponse responseModel = new EntryResponse(entryId, "a type");
+		ctx.json(responseModel);
+	}
+
+	public String convertObjectToXml(Object object) throws Exception {
+		return xmlMapper.writeValueAsString(object);
 	}
 
 	private Properties loadProperties() throws IOException {
